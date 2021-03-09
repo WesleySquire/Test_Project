0ea071799375e720f89f8383847562aa789d9806
EC2 Default User <ec2-user@ip-172-31-14-71.ec2.internal>
2021-03-09 15:18:27 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Test_Project
+
+Happy coding!
+
+Have a wonderful Day!
+fafawfawff
+fvfbtnbt
+dfefse
+ed33f3w4f3f3
+gsegfsg
+segesgse
---
+++ Sample.py
@@ -0,0 +1,8 @@
+
+fname = input(" Input first name: ")
+lnanme = input(" Input last name: ")
+print(" Hello {} {} I'm outside your house. :)".format(fname, lnanme))
+
+print(" {} you better hide.".format(fname))
+
+#comment
